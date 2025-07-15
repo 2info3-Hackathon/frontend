44e2589ae9b25fbeba18cc0874641b1d45505380
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2025-07-15 16:51:46 -0300

    FEAT: Começo de biologia #1

--- src/components/BotaoBiologia.vue
+++ src/components/BotaoBiologia.vue
@@ -9,8 +9,16 @@ function voltar() {
 </script>
 
 <template>
-  <div>
-    <h2>Grupo de Estudos de Biologia</h2>
+  <section>
+    <h1>Grupo de Estudos de Biologia</h1>
+
+    <div>
+        <h2>
+          Monitor:
+        </h2>
+        
+    </div>
+
     <button @click="voltar">Voltar</button>
-  </div>
+  </section>
 </template>
