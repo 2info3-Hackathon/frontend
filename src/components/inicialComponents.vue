96c60a1b1d259003be6dfee03a09584043dc42ea
agathahelena <agatha.helena.so@gmail.com>
2025-07-17 11:05:44 -0300

    FEAT : html terminado #9

--- src/components/inicialComponents.vue
+++ src/components/inicialComponents.vue
@@ -5,54 +5,71 @@ import App from '@/App.vue';
 <template>
 <header>
         <nav>
-          <h1>
-            <RouterLink to ="/">
-              IF
-            </RouterLink>
-          </h1>
-          <div class="search-wrapper">
-            <input type="text" class="search" placeholder="Buscar..." />
-          </div class="icons">
+          <div class="topo">
+            <p><img src="/public/imagem/Group 6.png" alt="if"></p>
+            <div class="pesquisa">
+                <input type="text" class="search" placeholder="Buscar..." />
+            </div>
           <ul>
-          <li><img src="" alt=""></li>
-          <li><img src="" alt=""></li>
+          <li><img class="if" src="/public/imagem/Group 40@2x.png" alt="user"></li>
+          <li><img src="/public/imagem/Group 1.png" alt="trasinhos"></li>
           </ul>
-        </nav>
-        <div class="banner">
-            <img src="/public/imagem/image 3.png" alt="banner" width="1000" height="200">
         </div>
-      <section>
-        <div>
+        </nav>
+        </header>
+      <section class="corpo">
+         <div class="banner">
+            <img src="/public/imagem/image 3.png" alt="banner" width="1000" height="250">
+        </div>
+        <div class="texto">
+          <h1>Bem Vindo ao site do ifc </h1>
+          <p>Aqui nós temos informações sobre : </p>
+        <div class="lista">
           <ul>
             <li>Vendas terceirão </li>
             <li>Núcleos Estudantis, Comissão e Comitês </li>
             <li>Grupos Estudantis </li>
             <li>Eventos ifc </li>
+            </ul>
+            <ul>
             <li>Atestados e Declarações </li>
             <li>Atendimentos</li>
             <li>CLIFC</li>
             <li>Contatos Importantes </li>
           </ul>
         </div>
+        </div>
       </section>
-</header>
 </template>
 
 <style scoped>
-header {
-display: flex;
-
-& nav {
-
+header nav div.topo{
+  display: flex;
+  background-color: rgba(236, 237, 238, 1);
+  justify-content: space-between;
 }
-& ul {
-  
+& nav div.pesquisa input{
+  margin: 30px 40px 0px 20px;
+  border-radius: 10px;
+  color: rgba(91, 163, 207, 1);
+}
+& nav div.topo ul {
+  display: flex;
+  list-style-type: none;
+}
+& nav div.topo ul li img{
+  margin: 0px 20px 0px 20px;
+}
+& div.banner img {
+    align-items: center;
+}
+section.corpo {
+  background-color:rgba(91, 163, 207, 1) ;
+}
+section.corpo div.texto {
+ border-radius: 20px;
+}
+section.corpo div.lista {
   display: flex;
 }
-
-& div.banner img {
-  margin: auto;
-}
-}
-
 </style>
