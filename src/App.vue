d6ba4f1cd437a9076d0b2957042e6cf053ae26d7
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2025-09-04 11:07:53 -0300

    FEAT: Começo da página login #13

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
-<script setup>
-
-</script>
-
 <template>
-  
+  <RouterView />
 </template>
 
 <style scoped>
---
+++ src/views/LoginComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <div class="principal">
+      <h1>oi</h1>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+.principal{
+  background: linear-gradient(135deg, #1c7ed4, #42a5f5);
+  margin: 50px 150px 50px 150px;
+  padding: 70px 10px 50px 10px;
+  border-radius: 20px;
+}
+</style>
