ea031d2ffaba8f0fe350b4e22361eafd5e7b62d0
agathahelena <agatha.helena.so@gmail.com>
2025-09-06 10:44:48 -0300

    FEAT :  só falta por pra baixo #10

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,70 +1,75 @@
 <script setup>
-
-
-
 </script>
 
 <template>
+    <footer>
 
-<footer>
+        <div class="conteudo">
 
-<div class="conteudo">
-
-<ul class="nomes">
-
-<li>
-    <strong>Desenvolvido por:</strong>
-</li>
-
-<li>
-
-    <p>Agatha H. S. de Oliveira</p>
-
-</li>
-
-<li>
-
-    <p>Ana Laura Simonato</p>
-
-</li>
-
-<li>
-
-    <p>Andressa Steffen</p>
-
-</li>
-
-<li>
-
-    <p>Gabriela “Yuri” H. Ramos</p>
-
-</li>
-
-<li>
-
-    <p>Kauan dos Santos L.</p>
-
-</li>
-
-<li>
-
-    <p>Milena K. da Rosa</p>
-
-</li>
-
-</ul>
-
-</div>
-
-</footer>
+            <ul class="nomes">
+                <li>
+                    <strong>Desenvolvido por:</strong>
+                </li>
+                <li>
+                    <p>Agatha H. S. de Oliveira</p>
+                </li>
+                <li>
+                    <p>Ana Laura Simonato</p>
+                </li>
+                <li>
+                    <p>Andressa Steffen</p>
+                </li>
+                <li>
+                    <p>Gabriela “Yuri” H. Ramos</p>
+                </li>
+                <li>
+                    <p>Kauan dos Santos L.</p>
+                </li>
+                <li>
+                    <p>Milena K. da Rosa</p>
+                </li>
+            </ul>
+            <ul class="outros">
+                <p>Endereço: Rodovia BR 280, Caixa Postal 21, <br> Centro. Araquari/SC. CEP: 89245-000
+                    Telefone: (47) 3803-7200p</p>
+                <p>
+                    E-mail: gabinete.araquari@ifc.edu.br
+                </p>
+                <p>
+                    Diretor(a): Fernando José Braz
+                </p>
+                <p>
+                    www.araquari.ifc.edu.br
+                </p>
+                <p>
+                    ifc.oficial.araquari
+                </p>
+            </ul>
+        </div>
+    </footer>
 
 </template>
 
 <style scoped>
+footer {
 
-footer div.conteudo ul.nomes{
-
-    
+    background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 1440 320' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath transform='scale(1,-1) translate(0,-320)' fill='%23ffffff' d='M0,288L30,261.3C60,235,120,181,180,165.3C240,149,300,171,360,181.3C420,192,480,181,540,170.7C600,160,660,150,720,154.7C780,160,840,181,900,192C960,203,1020,213,1080,208C1140,203,1200,171,1260,181.3C1320,192,1380,235,1410,261.3L1440,288L1440,320L0,320Z'/%3E%3C/svg%3E");
+    background-size: cover;
+    background-position: bottom;
+    background-repeat: no-repeat;
+    width: 102%;
+    height: 60vh;
+    background-color: #1C72D3;
+    display: block;
+    margin-left: -20px;
+    margin-bottom: -80px;
+    margin-top: 100px;
 }
-
+div.conteudo {
+    display: flex;
+    margin-top: 4vw;
+    margin-right: 8vw;
+    margin-left: 8vw;
+    justify-content: space-between
+}
 </style>
