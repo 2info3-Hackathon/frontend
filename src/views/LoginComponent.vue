2a3f73ed5c14b4d7df5400f9331ad1a40b41684a
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2025-09-07 22:58:40 -0300

    FEAT: Término da página principal #13

--- src/views/LoginComponent.vue
+++ src/views/LoginComponent.vue
@@ -1,7 +1,45 @@
 <template>
   <section>
     <div class="principal">
-      <h1>oi</h1>
+      <form>
+        <div class="icone">
+          <img src="/public/imagem/Group 71.png" alt="avatar">
+        </div>
+
+        <label for="user">Usuário:</label>
+        <input type="text" id="user" name="user" required>
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required>
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  </section>
+
+  <section class="cadastros">
+    <div>
+      <h2>Docente,</h2>
+      <p>
+        caso ainda não possua cadastro,<br> clique no link abaixo.
+      </p>
+      <a href="">Cadastre-se</a>
+    </div>
+
+    <div>
+      <h2>Dicente,</h2>
+      <p>
+        caso ainda não possua cadastro,<br> clique no link abaixo.
+      </p>
+      <a href="">Cadastre-se</a>
+    </div>
+
+    <div>
+      <h2>Terceirão Vendas,</h2>
+      <p>
+        caso ainda não possua cadastro,<br> clique no link abaixo.
+      </p>
+      <a href="">Cadastre-se</a>
     </div>
   </section>
 </template>
@@ -11,7 +49,99 @@
 .principal{
   background: linear-gradient(135deg, #1c7ed4, #42a5f5);
   margin: 50px 150px 50px 150px;
-  padding: 70px 10px 50px 10px;
+  padding: 150px 70px 90px 70px;
   border-radius: 20px;
+}
+
+form {
+  background: white;
+  padding: 85px 105px 35px 245px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  position: relative;
+}
+
+h2 {
+  font-weight: bolder;
+  font-size: 20px;
+  padding: 15px 5px 15px 0;
+}
+
+label {
+  font-size: 20px;
+  display: block;
+  margin-top: 10px;
+  font-weight: bold;
+}
+
+input {
+  font-size: 15px;
+  width: 80%;
+  padding: 15px 5px 15px 0;
+  margin-top: 5px;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+}
+
+form button {
+  background: linear-gradient(135deg, #1e88e5, #42a5f5);
+  color: white;
+  font-size: 18px;
+  padding: 10px 25px;
+  margin: 50px 150px 50px 189px;
+  border: none;
+  border-radius: 12px;
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+
+button:hover {
+  background: linear-gradient(135deg, #1565c0, #1e88e5);
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.25);
+  transform: scale(1.05);
+}
+
+.cadastros{
+  display: flex;
+}
+
+.cadastros div{
+  text-align: center;
+  margin: 0 7vw 5vw 9.5vw;
+}
+
+.cadastros p{
+  font-size: 18px;
+  padding: 10px 0 10px 0;
+}
+
+.cadastros a{
+  font-size: 18px;
+  color:#338ff8;
+  text-decoration: none;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+a:hover{
+  color: #0b4f9c;
+  text-decoration: underline;
+}
+
+.icone {
+  position: absolute;
+  top: -90px; /* sobe a imagem */
+  left: 50%;
+  transform: translateX(-50%);
+  background: #fff;
+  border-radius: 50%;
+  padding: 8px;
+  
+}
+
+.icone img {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
 }
 </style>
