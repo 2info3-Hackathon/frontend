d2d7039e4c8c714b5b866a008ada2380d861de02
agathahelena <agatha.helena.so@gmail.com>
2025-09-26 09:28:24 -0300

    FEAT : barra de pesquisa feita

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { ref } from "vue";
-import { useRouter } from 'vue-router'
+import { useRoute} from 'vue-router'
+import { computed, ref } from 'vue'
+import router from '@/router'
 
-const showDropdown = ref(false);
-const router = useRouter();
+const route = useRoute()
+const pesquisa = ref("")
+const indexAtivo = ref(-1)
 
 function irParaInicio(){
   router.push('/')
@@ -11,21 +13,88 @@ function irParaInicio(){
 function irParaLogin(){
   router.push('/login')
 }
+
+const navegacao = ref([
+  { id: "1", nome: "Informações importantes", rota: '/informacoes-importantesinformacoes' },
+  { id: "2", nome: "Grupo de estudos ", rota: '/grupo-estudos' },
+  {id:"3", nome:"Vendas terceirão", rota:'/terceirao'},
+  {id: "4", nome: "Biologia" , rota: '/biologia'},
+  {id: "5", nome: "Filosofia" , rota: '/filosofia'},
+  {id: "6", nome: "Física" , rota: '/fisica'},
+  {id: "7", nome: "Geografia" , rota: '/geografia'},
+  {id: "8", nome: "Hiostoria" , rota: '/historia'},
+  {id: "9", nome: "Matemática" , rota: '/matematica'},
+  {id: "10", nome: "Português" , rota: '/portugues'},
+  {id: "11", nome: "Química" , rota: '/quimica'},
+  {id: "12", nome: "Sociologia" , rota: '/sociologia'},
+  {id: "13", nome: "Núcleos" , rota: '/nucleos'},
+  {id: "14", nome: "Comissão" , rota: '/comissao'},
+  {id: "15", nome: "Cômite" , rota: '/comite'},
+  {id: "16", nome: "Atendimentos" , rota: '/atendimento'},
+  {id: "17", nome: "Contatos importantes" , rota: '/contatos-importantes'},
+  {id: "18", nome: "Atestados e declações" , rota: '/atestados-declaracoes'},
+  {id: "19", nome: "Clifc" , rota: '/clifc'},
+])
+
+const resultados = computed(() => {
+  const termo = pesquisa.value.trim().toLowerCase()
+
+  if (!termo) return []
+  return navegacao.value.filter(o =>
+    o.nome.toLowerCase().includes(termo)
+  )
+})
+function abrirNavegacao(navegacao){
+  if (!navegacao) return
+  router.push(navegacao.rota)
+  pesquisa.value = ""
+  indexAtivo.value = -1
+}
+function navegar(e) {
+  if (e.key === "ArrowDown") {
+    e.preventDefault()
+    indexAtivo.value = (indexAtivo.value + 1) % resultados.value.length
+  } else if (e.key === "ArrowUp") {
+    e.preventDefault()
+    indexAtivo.value = (indexAtivo.value - 1 + resultados.value.length) % resultados.value.length
+  } else if (e.key === "Enter") {
+    e.preventDefault()
+    if (indexAtivo.value >= 0) {
+      abrirNavegacao(resultados.value[indexAtivo.value])
+    } else if (resultados.value.length > 0) {
+      abrirNavegacao(resultados.value[0])
+    }
+  }
+} 
+
 </script>
 
 <template>
   <header>
-    <nav>
+ <nav>
       <div class="logo" @click="irParaInicio">
         <h1>
             IFC
         </h1>
         <span class="logo-title">Campus Araquari</span>
       </div>
-
-      <div class="barra">
-        <input type="text" class="search" placeholder="Buscar..." />
-      </div>
+   <div class="pesquisa">
+        <i class="bi bi-search"></i>
+      <input type="text"
+      v-model="pesquisa"
+      placeholder="Pesquisar..."
+      @keydown="navegar"/>
+      <div class="resultados" v-if="resultados.length">
+  <ul>
+    <li v-for="(item, index) in resultados"
+        :key="item.id"
+        :class="{ ativo: index === indexAtivo }"
+        @click="abrirNavegacao(item)">
+      {{ item.nome }}
+    </li>
+  </ul>
+</div>
+    </div>
       <ul class="menu">
         <span class="mdi mdi-account" @click="irParaLogin"></span>
 
@@ -71,6 +140,10 @@ header nav {
   padding: 0.1vw 0 0 1vw;
   font-size: 1vw;
 }
+.pesquisa{
+margin-left:45vw;
+margin-right:-17vw;
+}
 
 input {
   width: 400px;
@@ -80,6 +153,32 @@ input {
   border: 0;
   background-color: #f1f1f1;
   padding: 5px;
+}
+.resultados {
+  position: absolute;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  width: 15.9vw;
+  margin-left: 8px;
+  z-index: 999;
+  background-color: #f1f1f1;
+}
+
+.resultados ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.resultados li {
+  padding: 8px 12px;
+  cursor: pointer;
+  color: black
+}
+
+.resultados li:hover,
+.resultados li.ativo {
+  background-color: #0090E3;
 }
 
 .barra::before {
